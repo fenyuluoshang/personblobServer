2c8a142662648915a7fee5fd74baf27fb287bd00
纷羽 <gzj0078@icloud.com>
2019-02-12 22:21:18 +0800

    debug

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,7 +1,7 @@
 const db = require('./db')
 const router = require('express').Router()
 
-router.post('webhook', (req, res, next) => {
+router.post('/webhook', (req, res, next) => {
     console.log(req.body)
     res.json({
         success: 1,
